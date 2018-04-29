020dd4dbd24f5b7acf51922940939d4316fb3212
Kent Gruber <kgruber1@emich.edu>
2018-04-29 14:50:06 -0400

    add API documentation link to source for latest_feed client method

--- lib/falconz/apis/feed.rb
+++ lib/falconz/apis/feed.rb
@@ -2,6 +2,7 @@ module Falconz
   module APIs
     module Feed
       # access a feed of last 250 reports over 24h
+      # https://www.hybrid-analysis.com/docs/api/v2#/Feed/get_feed_latest
       def latest_feed(**options)
         get_request("/feed/latest", options)
       end
