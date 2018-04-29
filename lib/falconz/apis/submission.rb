f2b6f6183c348d39e2c35e80343a7653854e59ad
Kent Gruber <kgruber1@emich.edu>
2018-04-29 17:00:33 -0400

    cleanup submit API module

--- lib/falconz/apis/submission.rb
+++ lib/falconz/apis/submission.rb
@@ -2,6 +2,7 @@ module Falconz
   module APIs
     module Submission 
       # submit a local file for analysis
+      # https://www.hybrid-analysis.com/docs/api/v2#/Submission/post_submit_file
       def submit_file(**options)
         options[:file] = File.open(options[:file], "r")
         response = post_request("/submit/file", options)
@@ -10,16 +11,26 @@ module Falconz
       end
      
       # submit a file by url for analysis 
+      # https://www.hybrid-analysis.com/docs/api/v2#/Submission/post_submit_url_to_file
       def submit_file_by_url(**options)
         post_request("/submit/url-to-file", options)
       end
       
-      # submit a url for analysis 
+      # submit a url for analysis
+      # https://www.hybrid-analysis.com/docs/api/v2#/Submission/post_submit_url_for_analysis
       def submit_url(**options)
         post_request("/submit/url-for-analysis", options)
       end
+      
+      # determine a SHA256 that an online file or URL submission will 
+      # have when being processed by the system. Note: this is useful when looking up URL analysis
+      # https://www.hybrid-analysis.com/docs/api/v2#/Submission/post_submit_hash_for_url
+      def hash_for_url(url)
+        post_request("/submit/url-for-analysis", url: url)
+      end
      
-      # submit dropped file for analysis 
+      # submit dropped file for analysis
+      # https://www.hybrid-analysis.com/docs/api/v2#/Submission/post_submit_dropped_file
       def submit_dropped_file(**options)
         post_request("/submit/dropped-file", options)
       end
@@ -28,6 +39,7 @@ module Falconz
       # submission will have when being processed by 
       # the system. Note: this is useful when looking 
       # up URL analysis
+      # https://www.hybrid-analysis.com/docs/api/v2#/Submission/post_submit_hash_for_url
       def hash_from_url(**options)
         post_request("/submit/hash-for-url", options)
       end
