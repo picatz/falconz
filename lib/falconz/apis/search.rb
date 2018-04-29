516a02a869cc6d26e2c1a8a43a4a29df093644a2
Kent Gruber <kgruber1@emich.edu>
2018-04-29 18:17:33 -0400

    fix missing search API module
    wtf was I thinking?

--- lib/falconz/apis/search.rb
+++ lib/falconz/apis/search.rb
@@ -3,24 +3,24 @@ module Falconz
     module Search
       # summary for given hash
       # https://www.hybrid-analysis.com/docs/api/v2#/Search/post_search_hash
-      def search_hash(string, **options)
-        raise "Requires a MD5, SHA1 or SHA256 hash" if string.nil?
-        options[:hash] = string
-        post_request("/search/hash", options)
+      def hash(string, **options)
+        options[:hash] = string unless string.nil?
+        raise "Requires a MD5, SHA1 or SHA256 hash" if options[:hash].nil?
+        get_request("/key/current", options)
       end
       
       # summary for given hashes
       # https://www.hybrid-analysis.com/docs/api/v2#/Search/post_search_hashes
-      def search_hashes(*strings, **options)
-        raise "Requires at least one MD5, SHA1 or SHA256 hash" if strings.nil? or strings.empty?
-        options[:hashes] = strings
-        post_request("/search/hashes", options)
+      def hashes(*strings, **options)
+        options[:hashes] = strings unless strings.nil? or strings.empty?
+        raise "Requires MD5, SHA1 or SHA256 hashes" if options[:hashes].nil?
+        get_request("/search/hashes", options)
       end
      
       # search the database using search terms
       # https://www.hybrid-analysis.com/docs/api/v2#/Search/post_search_terms
-      def search_terms(**options)
-        post_request("/search/terms", options)
+      def terms(**options)
+        get_request("/search/terms", options)
       end
     end
   end
