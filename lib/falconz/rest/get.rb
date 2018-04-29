b975572febf36f3c410395c0008325aaf42b0330
Kent Gruber <kgruber1@emich.edu>
2018-04-29 16:07:19 -0400

    cleanup get_request method by removing options
    this also remove the weird json argument

--- lib/falconz/rest/get.rb
+++ lib/falconz/rest/get.rb
@@ -1,15 +1,14 @@
 module Falconz
   module REST
+    # HTTP 1.1 GET request method to make on the API endpoint.
+    #
+    # This is a module that is used in pretty much all the API
+    # modules in order to talk to the API endpoint.
     module GET
-      def get_request(path, json: false, **options)
+      def get_request(path)
         response = HTTParty.get(url + path, headers: header)
-        if response.success?
-          return response.body if json
-          return response 
-        else
-          binding.pry
-          raise response.to_h
-        end
+        return response if response.success?
+        raise response
       end
     end
   end
