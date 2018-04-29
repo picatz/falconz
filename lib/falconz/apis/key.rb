541c1b452adf1bc12ba980923cad1eaed01466f5
Kent Gruber <kgruber1@emich.edu>
2018-04-29 14:50:54 -0400

    add API documentation link to source for current_key client method

--- lib/falconz/apis/key.rb
+++ lib/falconz/apis/key.rb
@@ -2,6 +2,7 @@ module Falconz
   module APIs
     module Key 
       # return information about the used API key
+      # https://www.hybrid-analysis.com/docs/api/v2#/Key/get_key_current
       def current_key(**options)
         get_request("/key/current", options)
       end
